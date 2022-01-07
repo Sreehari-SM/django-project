e09bd7925c237f03f32f3d22276099750a543ce7
Sreehari-SM <sreeharisunil9711@gmail.com>
2022-01-07 20:44:46 +0530

    first commit

---
+++ src/dashboard/web/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from web.models import Product, Task
+
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Task)
---
+++ src/dashboard/web/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from web.views import index
+
+app_name = "web"
+urlpatterns = [
+    path("",index,name="index"),
+]
